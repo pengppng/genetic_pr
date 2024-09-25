310c238a98500e078c260f349a326aafc36c0923
pengppng <pannapor.boo@gmail.com>
2024-09-25 23:23:50 +0700

    first commit

---
+++ random_gene.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_random_dna_sequence(length=30):
+    dna_bases = ['A', 'T', 'C', 'G']  # ตัวอักษรที่เราต้องการสุ่ม
+    random_sequence = ''.join(random.choice(dna_bases) for _ in range(length))
+    return random_sequence
+
+# สุ่ม DNA sequence 30 ตัวอักษร
+dna_sequence = generate_random_dna_sequence(30)
+print("Random DNA Sequence:", dna_sequence)
